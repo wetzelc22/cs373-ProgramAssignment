d8dfe9f782d68e52550289b417f973c6b89843b9
Chris <cwetzel2@binghamton.edu>
2019-09-15 22:38:19 -0400

    finished program assign1

--- wetzel_p1.c
+++ wetzel_p1.c
@@ -17,41 +17,42 @@ struct transition{
 };
 
 
-int * runAutomata(struct state * states[], struct transition * transitions[], char * inputStr, int max, int start, int startInd);
-struct transition * findTransition(struct transition * transitions[], int s, char c);
-int findStartState(struct state * states[]);
-int findStateIndex(struct state * states[], int cs);
+int * runAutomata(struct state * states[], struct transition * transitions[], char * inputStr, int max, int start, int startInd, int stateLen, int tranLen);
+struct transition * findTransition(struct transition * transitions[], int s, char c, int len);
+int findStartState(struct state * states[], int len);
+int findStateIndex(struct state * states[], int cs, int stateLen);
 
 
 
-int * runAutomata(struct state * states[], struct transition * transitions[], char * inputStr, int max, int start, int startInd){
-		char accept[] = "accept";
-		char reject[] = "reject";
+int * runAutomata(struct state * states[], struct transition * transitions[], char * inputStr, int max, int start, int startInd, int stateLen, int tranLen){
+		char accept[] = "accept\n";
+		char reject[] = "reject\n";
 		char * temp = inputStr;
-		int tranTrack = 1;
+		int tranTrack = 2;
 		int ptr = 0;
-		int cs = start;
+		int cs = states[startInd]->val;
 		int csIndex = startInd;
-		int * stateTrack = (int *)malloc(2 * sizeof(int));
-		stateTrack[0] = 0;
-		stateTrack[1] = start;
-		while(tranTrack < (max+1)){
+		int * stateTrack = (int *)malloc(3 * sizeof(int));
+		stateTrack[0] = tranTrack;
+		stateTrack[1] = 0;
+		stateTrack[2] = start;
+		while(tranTrack < (max+2)){
 			tranTrack++;
 			char cur = inputStr[ptr];
-			struct transition * t = findTransition(transitions, cs, cur);
+			struct transition * t = findTransition(transitions, cs, cur, tranLen);
 			temp[ptr] = t->a;
 			cs = t->after;
 			stateTrack = realloc(stateTrack, (tranTrack+1)*sizeof(int));
 			stateTrack[tranTrack] = cs;
-			csIndex = findStateIndex(states, cs);
+			csIndex = findStateIndex(states, cs, stateLen);
 			int check = strcmp(accept, states[csIndex]->type);
 			if(check == 0){
-				stateTrack[0] = 1;
+				stateTrack[1] = 1;
 				break;
 			}
 			check = strcmp(reject, states[csIndex]->type);
 			if(check == 0){
-				stateTrack[0] = 2;
+				stateTrack[1] = 2;
 				break;
 			}
 			if(t->dir == 'R'){
@@ -60,49 +61,39 @@ int * runAutomata(struct state * states[], struct transition * transitions[], ch
 				ptr--;
 			}
 		}
-		free(temp);
+		stateTrack[0] = tranTrack;
 		return stateTrack;
 
 }
 
-struct transition *findTransition(struct transition * transitions[], int s, char c){
-	printf("in findTran");
-	struct transition * ret;
-	int run = 0;
+struct transition *findTransition(struct transition * transitions[], int s, char c, int len){
+	struct transition * ret = malloc(sizeof(struct transition));
 	int i = 0;
-	while(run == 0){
+	for(i = 0; i < len; i++){
 		if(transitions[i]->start == s && transitions[i]->b == c){
 			ret = transitions[i];
-			run = 1;
+			break;
 		}
 	}
 	return ret;	
 }
 
-int findStartState(struct state * states[]){
-	printf("In findStartState");	
-	bool b = true;
+int findStartState(struct state * states[], int len){	
 	int i = 0;
-	char start[] = "start";
-	while(b){
+	char start[] = "start\n";
+	for(i = 0; i < len; i++){
 		int check = strcmp(start, states[i]->type);
 		if(check == 0){
-			b = false;
-		}else{
-			i++;
+			break;
 		}
 	}
 	return i;
 }
-int findStateIndex(struct state * states[], int cs){
-	printf("in findStateIndex");
-	bool b = true;
+int findStateIndex(struct state * states[], int cs, int stateLen){
 	int i = 0;
-	while(b){
+	for(i=0;i < stateLen; i++){
 		if(states[i]->val == cs){
-			b = false;
-		}else{
-			i++;
+			break;
 		}
 	}
 	return i;
@@ -111,7 +102,7 @@ int findStateIndex(struct state * states[], int cs){
 int main(int argc, char **argv){
 	FILE *fptr;
 	char line[128];
-	int length = sizeof(argv[2]);
+	int length = 1000;
 	char inputStr[length];
 	strcpy(inputStr, argv[2]);
 	int max = atoi(argv[3]);
@@ -120,7 +111,7 @@ int main(int argc, char **argv){
 	fptr = fopen(argv[1], "r");
 	struct state * states[1000];
 	struct transition *transitions[100000];
-	while(fgets(line, sizeof(line), fptr) != NULL){
+	while(fgets(line, sizeof(line), fptr)){
 		int count = 0;
 		char *s = strtok(line, "\t");
 		char *state = "state";
@@ -147,31 +138,26 @@ int main(int argc, char **argv){
 			struct transition *t = malloc(sizeof(struct transition));
 			while(s){
 				if(count ==1){
-					//printf("1: %s\n", s);
 					count++;
 					int i = atoi(s);
 					t->start = i;
 					s = strtok(NULL, "\t");
 				}else if(count == 2){
-					//printf("2: %s\n", s);
 					count++;
 					char c = s[0];
 					t->b = c;
 					s = strtok(NULL, "\t");
 				}else if(count == 3){
-					//printf("3: %s\n", s);
 					count++;
 					int n = atoi(s);
 					t->after = n;
 					s = strtok(NULL, "\t");
 				}else if(count == 4){
-					//printf("4: %s\n", s);
 					count++;
 					char c = s[0];
 					t->a = c;
 					s = strtok(NULL, "\t");
 				}else if(count == 5){
-					//printf("5: %s\n", s);
 					char dir = s[0];
 					t->dir = dir;
 					s = strtok(NULL, "\t");
@@ -181,7 +167,6 @@ int main(int argc, char **argv){
 				}
 		
 			}
-			printf("tranInd: %d", tranInd);
 			transitions[tranInd] = t;
 			tranInd++;
 			int checkB = 0;
@@ -193,37 +178,44 @@ int main(int argc, char **argv){
 			if(checkB == 0){
 				struct state * st = malloc(sizeof(struct state));
 				st->val = t->start;
-				st->type = NULL;
+				st->type = "none";
 				states[stateInd] = st;
 				stateInd++;
-				printf("check here\n");
 			}
 			if(checkA == 0){
 				struct state * st = malloc(sizeof(struct state));
 				st->val = t->after;
-				st->type = NULL;
+				st->type = "none";
 				states[stateInd] = st;
 				stateInd++;
 			}
 		}
 	}
 	fclose(fptr);
-	int startIndex = findStartState(states);
+	int startIndex = findStartState(states, stateInd-1);
 	int startStateVal = states[startIndex]->val;
-	int * arr = runAutomata(states, transitions, inputStr, max, startStateVal, startIndex);
-	int siz = sizeof(arr)/4;
-	for(int i = 1; i < siz; i++){
-		if(i < (siz-1)){
-			printf("%c->", arr[i]);
+	int * arr = runAutomata(states, transitions, inputStr, max, startStateVal, startIndex, stateInd-1, tranInd);
+	int len = arr[0];
+	for(int i = 2; i < len+1; i++){
+		if(i < (len)){
+			printf("%d->", arr[i]);
 		}else{
-			if(arr[0] == 0){
-				printf("%d quit",arr[i]);
-			}else if(arr[0] == 1){
-				printf("%d accept",arr[i]);
+			if(arr[1] == 0){
+				printf("%d quit\n",arr[i]);
+			}else if(arr[1] == 1){
+				printf("%d accept\n",arr[i]);
 			}else{
-				printf("%d reject", arr[i]);
+				printf("%d reject\n", arr[i]);
 			}
 		}
+	}
+	free(arr);
+	for(int i = 0; i<stateInd; i++){
+		//free(states[i]->type);
+		free(&states[i]);
+	}
+	for(int i = 0; i<tranInd; i++){
+		free(&transitions[i]);
 	}
 	return 0;
 }
